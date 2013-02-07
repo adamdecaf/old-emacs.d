9552a6aa76ef648393a490587295431a28010790
Adam Shannon <adam@ashannon.us>
2013-02-06 22:37:56 -0600

    Adding tramp for servers.

--- mine/mine-commands.el
+++ mine/mine-commands.el
@@ -1,5 +1,19 @@
 ;;  Special Commands
 
+;; Sudo edit a local file
 (defun sudo-edit-file ()
   (interactive)
   (find-file "/sudo:root@localhost:/"))
+
+;; Edit remote files
+(defun ssh-edit-achilles ()
+  (interactive)
+  (find-file "/scp:ubuntu@54.235.123.121:/home/ubuntu/"))
+
+(defun ssh-edit-mercury ()
+  (interactive)
+  (find-file "/scp:ec2-user@107.20.179.53:/home/ec2-user/"))
+
+(defun ssh-edit-orion ()
+  (interactive)
+  (find-file "/scp:ubuntu@54.235.214.21:/home/ubuntu/"))
